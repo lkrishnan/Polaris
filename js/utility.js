a08edaeb15ca9fa3145040864fd26566f95cec3f
lakkrishnan <lakshmanank@gmail.com>
2019-08-22 14:46:15 -0400

    owner number problem fix

--- js/utility.js
+++ js/utility.js
@@ -490,16 +490,16 @@ TextToGeom = {
 },
 
 Utils = {
-	compareValues: function( key, order="asc" ){
+	compareValues: function( key, order ){
 		return function(a, b){
 			if( !a.hasOwnProperty( key ) || !b.hasOwnProperty( key ) ){
 				return 0; 
 			}
     
-			const varA = ( typeof a[ key ] === 'string' ) ? a[ key ].toUpperCase( ) : a[ key ];
-			const varB = ( typeof b[ key ] === 'string' ) ? b[ key ].toUpperCase( ) : b[ key ];
+			var varA = ( typeof a[ key ] === 'string' ) ? a[ key ].toUpperCase( ) : a[ key ];
+			var varB = ( typeof b[ key ] === 'string' ) ? b[ key ].toUpperCase( ) : b[ key ];
       
-			let comparison = 0;
+			var comparison = 0;
 			
 			if( varA > varB ){
 				comparison = 1;
